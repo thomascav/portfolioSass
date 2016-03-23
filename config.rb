da36afd1a0736e1d04018f13e5030e7f4094c779
thomascav <tcavalar@numericable.fr>
2016-03-23 16:55:54 +0100

    installed font-awesome gem

--- config.rb
+++ config.rb
@@ -9,6 +9,7 @@ images_dir = "img"
 javascripts_dir = "javascripts"
 
 require 'bootstrap-sass'
+require 'font-awesome-sass'
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
